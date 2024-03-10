1e9c7eaca8cd8ab280b6a1cf91585313d8c9bcd5
taherelzoghby <taheramin442@gmail.com>
2024-03-10 13:47:54 +0200

    edited cubit

--- lib/features/home/presentation/view_model/get_products_cubit/get_products_cubit.dart
+++ lib/features/home/presentation/view_model/get_products_cubit/get_products_cubit.dart
@@ -4,6 +4,7 @@ import 'package:slash_task/core/errors/failure_message.dart';
 import 'package:slash_task/core/models/product_model/product_model.dart';
 import 'package:slash_task/features/home/data/repos/home_repo.dart';
 import 'package:meta/meta.dart';
+
 part 'get_products_state.dart';
 
 class GetProductsCubit extends Cubit<GetProductsState> {
@@ -11,15 +12,27 @@ class GetProductsCubit extends Cubit<GetProductsState> {
 
   GetProductsCubit(this.homeRepo) : super(GetProductsInitial());
 
-  getProducts() async {
-    emit(GetProductsLoading());
-    Either<FailureServ, List<ProductModel>> result =
-        await homeRepo.getProducts();
+  getProducts({int pagNum = 1}) async {
+    if (pagNum == 1) {
+      emit(GetProductsLoading());
+    } else {
+      emit(GetProductsLoadingPagination());
+    }
+
+    Either<FailureServ, List<ProductModel>> result = await homeRepo.getProducts(
+      pagNum: pagNum,
+    );
     result.fold(
       (failure) {
-        emit(
-          GetProductsError(message: failure.message),
-        );
+        if (pagNum == 1) {
+          emit(
+            GetProductsError(message: failure.message),
+          );
+        } else {
+          emit(
+            GetProductsErrorPagination(message: failure.message),
+          );
+        }
       },
       (products) {
         emit(

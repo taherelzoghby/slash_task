f124b967bfd825a36ba9cd5985c5a5776ae6e262
taherelzoghby <taheramin442@gmail.com>
2024-03-10 14:05:13 +0200

    added cubit for get product in details page

--- lib/features/details/presentation/view_model/get_product_cubit/get_product_cubit.dart
+++ lib/features/details/presentation/view_model/get_product_cubit/get_product_cubit.dart
@@ -1,12 +1,30 @@
 import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
 import 'package:meta/meta.dart';
+import 'package:slash_task/core/errors/failure_message.dart';
 import 'package:slash_task/core/models/product_model/product_model.dart';
+
+import '../../../data/repos/details_repo.dart';
 
 part 'get_product_state.dart';
 
 class GetProductCubit extends Cubit<GetProductState> {
-  GetProductCubit() : super(GetProductInitial());
-  getProduct(int id){
+  final DetailsRepo detailsRepo;
 
+  GetProductCubit(this.detailsRepo) : super(GetProductInitial());
+
+  getProduct(int id) async {
+    emit(GetProductLoading());
+    Either<FailureServ, ProductModel> result = await detailsRepo.getProduct(
+      id: id,
+    );
+    result.fold(
+      (failure) => emit(
+        GetProductFailure(message: failure.message),
+      ),
+      (product) => emit(
+        GetProductLoaded(productModel: product),
+      ),
+    );
   }
 }

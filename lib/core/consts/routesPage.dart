94ae926d1156ad0c9a61a8c5e7a968a841257c67
taherelzoghby <taheramin442@gmail.com>
2024-03-10 10:03:31 +0200

    added cubit to home route

--- lib/core/consts/routesPage.dart
+++ lib/core/consts/routesPage.dart
@@ -1,5 +1,9 @@
+import 'package:dio/dio.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:slash_task/core/consts/api_service.dart';
+import 'package:slash_task/features/home/data/repos/home_repo_impl.dart';
+import 'package:slash_task/features/home/presentation/view_model/get_products_cubit/get_products_cubit.dart';
 import '../../features/details/presentation/view/details_view.dart';
 import '../../features/details/presentation/view_model/arrow_cubit/arrow_disc_cubit.dart';
 import '../../features/details/presentation/view_model/change_color_cubit/change_color_or_image_cubit.dart';
@@ -7,9 +11,7 @@ import '../../features/details/presentation/view_model/change_image_cubit/change
 import '../../features/details/presentation/view_model/change_material_cubit/change_material_cubit.dart';
 import '../../features/details/presentation/view_model/change_size_cubit/change_size_cubit.dart';
 import '../../features/home/presentation/view/home_view.dart';
-
 import '../helper/custom_animation.dart';
-
 const homePath = '/';
 const detailsPath = '/detailsPage';
 final router = GoRouter(
@@ -19,7 +21,14 @@ final router = GoRouter(
       pageBuilder: (context, state) => buildPageWithDefaultTransition(
         context: context,
         state: state,
-        child: const HomeView(),
+        child: BlocProvider(
+          create: (_) => GetProductsCubit(
+            HomeReoImpl(
+              apiService: ApiService(dio: Dio()),
+            ),
+          ),
+          child: const HomeView(),
+        ),
       ),
     ),
     GoRoute(
